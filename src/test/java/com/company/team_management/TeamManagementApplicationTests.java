05c88325e39c601434af530bca4b8f5326244ece
Volodymyr Polishchuk <volodimirpolishchuck@gmail.com>
2023-08-21 21:59:53 +0300

    first commit - project initialization

---
+++ src/test/java/com/company/team_management/TeamManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.team_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
